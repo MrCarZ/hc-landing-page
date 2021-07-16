9983c59c1dc00716681f16764d52388a24a78ead
Carz Eduardo <eduardovinagre123@gmail.com>
2021-07-16 20:09:01 -0300

    Write feedback messages for form data

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
 const handleButton = (email) => {
+  const feedbackMessage = document.getElementById("input-feedback");
+  feedbackMessage.style.color = "#FFFFFF";
+  feedbackMessage.textContent = "Waiting for response...";
   const isValid = handleEmailValidity(email);
 
   if (isValid) {
@@ -6,18 +9,18 @@ const handleButton = (email) => {
     if (data != null) {
       const alreadyHasEmail = data.filter((value) => value === email.value);
       if (alreadyHasEmail.length >= 1) {
-        // generate text for already used email
+        alreadyUsedMessage(feedbackMessage);
       } else {
         data.push(email.value);
+        successMessage(feedbackMessage);
       }
     } else {
       data = [email.value];
+      successMessage(feedbackMessage);
     }
     localStorage.setItem("customer-info", JSON.stringify(data));
   } else {
-    console.log(
-      "this is not a valid e-mail, please insert another email address"
-    );
+    invalidMessage(feedbackMessage);
   }
 };
 
@@ -25,3 +28,30 @@ const handleEmailValidity = (email) =>
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
     email.value
   );
+
+const generateHTMLElement = (tag, atributes) => {
+  const newElement = document.createElement(tag);
+  const properties = Object.getOwnPropertyNames(atributes);
+  properties.map((value) => {
+    newElement[`${value}`] = atributes[`${value}`];
+  });
+  return newElement;
+};
+
+const successMessage = (feedbackMessage) => {
+  feedbackMessage.textContent = "Success! Done! ;)";
+  feedbackMessage.style.color = "#00FF00";
+};
+
+const alreadyUsedMessage = (feedbackMessage) => {
+  feedbackMessage.textContent =
+    "This e-mail has already been used. Please check your mailbox.";
+  feedbackMessage.style.color = "#FF0000";
+};
+
+const invalidMessage = (feedbackMessage) => {
+  feedbackMessage.textContent =
+    "This e-mail is invalid. Please type a valid one.";
+  feedbackMessage.style.color = "#FF0000";
+};
+
